2c9a66a0865deeb1ad226d1701b23e2f4e3e602e
gate1253 <gway1253@gmail.com>
2025-11-09 05:04:01 +0900

    인증 방식 변경

--- member/auth.js
+++ member/auth.js
@@ -2,7 +2,7 @@
 // IMPORTANT: replace CLIENT_ID with your Google OAuth Client ID
 (function(){
   // 구성: 반드시 실제 값으로 교체
-  const CLIENT_ID = '178229425055-tc4b6f766ukm17jdd6blt9t5oa13gcea.apps.googleusercontent.com';
+  const CLIENT_ID = '178229425055-dgojh3m0b58rbf1uu6qk30jtbj1ne8lh.apps.googleusercontent.com';
   const REDIRECT_URI = (location.origin + '/member/callback.html');
   const SCOPE = 'openid email profile';
   const TOKEN_ENDPOINT = 'https://oauth2.googleapis.com/token';
